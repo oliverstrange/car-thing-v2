31f666f071d44b9a4d15cb2650cf48345c4ad2e1
oliverstrange <oliver.m.w.strange@gmail.com>
2025-12-07 16:17:51 +0000

    update arc to start at top

--- handlers/storage.py
+++ handlers/storage.py
@@ -2,3 +2,6 @@ class StorageHandler:
     def __init__(self, app):
         print("StorageHandler initialized")
         self.app = app
+        print(self.app.dark_mode, type(self.app.dark_mode))
+        print(self.app.accent_color, type(self.app.accent_color))
+        print(self.app.start_screen_index, type(self.app.start_screen_index))
